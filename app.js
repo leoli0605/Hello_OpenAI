7d1f537305c0a199e770459aa4493ccbaf35691e
Leo CH Li <lli31@logitech.com>
2023-10-24 11:03:16 +0800

    添加如何使用 Chat, DALL-E 的程式碼範例

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const OpenAI = require("openai");
+
+async function main() {
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  /* This is the example how to chat with GPT-3 */
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: "Say this is a test" }],
+    model: "gpt-3.5-turbo",
+  });
+  console.log(chatCompletion);
+  console.log(chatCompletion.choices[0]);
+  /* This is the example how to generate image with DALL-E */
+  const response = await openai.images.generate({
+    prompt: "a white siamese cat",
+    n: 1,
+    size: "1024x1024",
+  });
+  image_url = response.data[0].url;
+  console.log(image_url);
+}
+
+main();
